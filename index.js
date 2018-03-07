a511ada5b49bb1c5cc71749cdf73014204a88058
Terry Blue <terryblue@comcast.net>
2018-03-07 01:22:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
 xhr.responseType = 'json';
 
 xhr.onload = function() {
-  console.log(xhr.response);
+  console.log("xhr.response", xhr.response);
 };
- 
+
 xhr.onerror = function() {
   console.log('Booo');
 };
