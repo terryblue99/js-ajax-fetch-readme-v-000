e0f34856d325d3e219607ecdcca803a6cc0efc47
Terry Blue <terryblue@comcast.net>
2018-03-07 02:23:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much.";
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+//
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// };
+//
+// xhr.onerror = function() {
+//   console.log('Booo');
+// };
+//
+// xhr.send();
+
+//'fetch' produces the same output as the above XHR code
+const token = '3c22ddb84606fadf0e6da86add01981582dcd559'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+//The authorization token was generated at https://github.com/settings/tokens.
+//The line then(res => res.json()) is getting the response res from fetch and
+//using the json method to turn it into JSON. Then it's passing the JSON to the
+//next line, then(json => console.log(json)), to be handled by that function
