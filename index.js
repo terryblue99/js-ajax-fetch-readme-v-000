6424481c932e4149a320dce335e5d28a32231f36
Terry Blue <terryblue@comcast.net>
2018-03-07 01:40:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const app = "I don't do much.";
-// let xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
-// xhr.responseType = 'json';
-// 
-// xhr.onload = function() {
-//   console.log(xhr.response);
-// };
-// 
-// xhr.onerror = function() {
-//   console.log('Booo');
-// };
-// 
-// xhr.send();
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log('Booo');
+};
+
+xhr.send();
 
 fetch('https://api.github.com/repos/jquery/jquery/commits')
-  .then(res => res.json())
+  .then(res => res.json()) // give me the body of the response parsed as JSON
   .then(json => console.log(json));
-
