7c622af01de317b1db0acdb6cbe81c5647aa2cdf
Terry Blue <terryblue@comcast.net>
2018-03-07 02:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,4 @@ fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
   }
-}).then(res => res.json())
-  .then(json => console.log(json));
+}).then(res => res.json()).then(json => console.log(json));
