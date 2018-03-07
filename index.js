808ad419513a7a6b0fe71588ca4e3781f0c584cb
Terry Blue <terryblue@comcast.net>
2018-03-07 01:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@
 
 const token = '6948be3ff488f402f37bae2eac8dcc6694d4de6d'
 // fetch produces the same output as the above XHR code
-fetch('https://api.github.com/user/repos')
-  .then(res => res.json()) // give me the body of the response parsed as JSON
-  .then(json => console.log(json))
-  
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
